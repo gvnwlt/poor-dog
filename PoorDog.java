2988bfebb1ff03074adc28b317219e9d6f19d986
G <gvnwltrs@protonmail.com>
2019-10-05 22:32:11 -0400

    Add files via upload

---
+++ PoorDog.java
@@ -0,0 +1,25 @@
+class PoorDog
+{
+	public static void main(String[] args) {
+		PoorDog sizeAndName = new PoorDog(); 
+		
+
+		sizeAndName.getSize();
+		sizeAndName.getName();
+
+        }
+
+        private int size; 
+	private String name; 
+
+	public int getSize()
+	{
+		return size; 
+	} 
+
+	public  String getName()
+	{
+		return name; 
+	} 
+}
+
